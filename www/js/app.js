57295e4366992a47708176d0625425df6599a954
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2023-12-03 07:11:43 +0700

    add some features

--- www/js/app.js
+++ www/js/app.js
@@ -8,15 +8,41 @@ function tampilkanDaftarBarang() {
   // Simpan daftar barang dalam Local Storage
   var daftarBarang = JSON.parse(localStorage.getItem('daftarBarang')) || [];
 
-  // Tampilkan daftar barang pada halaman
-  var listBarang = document.getElementById('listBarang');
-  listBarang.innerHTML = '';
+  // Tampilkan daftar barang pada tabel
+  var tabelBarang = document.getElementById('tabelBarang');
+  var tbody = document.getElementById('listBarang');
+  tbody.innerHTML = '';
 
-  daftarBarang.forEach(function (barang) {
-    var listItem = document.createElement('li');
-    listItem.textContent = `Nama: ${barang.nama}, Harga: ${barang.harga}, Stok: ${barang.stok}`;
-    listBarang.appendChild(listItem);
-  });
+  if (daftarBarang.length === 0) {
+    // Jika daftar barang kosong, tambahkan pesan
+    var row = tbody.insertRow();
+    var cellInfo = row.insertCell(0);
+    cellInfo.colSpan = 5;
+    cellInfo.textContent = 'Belum ada barang yang ditambahkan.';
+  } else {
+    daftarBarang.forEach(function (barang, index) {
+      var row = tbody.insertRow();
+      var cellNomor = row.insertCell(0);
+      var cellNama = row.insertCell(1);
+      var cellHarga = row.insertCell(2);
+      var cellStok = row.insertCell(3);
+      var cellAksi = row.insertCell(4);
+
+      cellNomor.textContent = index + 1;
+      cellNama.textContent = barang.nama;
+      cellHarga.textContent = barang.harga;
+      cellStok.textContent = barang.stok;
+
+      // Tambahkan tombol hapus dengan fungsi hapusData
+      var buttonHapus = document.createElement('button');
+      buttonHapus.textContent = 'Hapus';
+      buttonHapus.addEventListener('click', function () {
+        hapusData(index);
+      });
+
+      cellAksi.appendChild(buttonHapus);
+    });
+  }
 }
 
 function simpanData() {
@@ -51,4 +77,16 @@ function simpanData() {
   document.getElementById('stok').value = '';
 
   alert('Data barang berhasil disimpan.');
+}
+
+function hapusData(index) {
+  // Hapus item dari Local Storage
+  var daftarBarang = JSON.parse(localStorage.getItem('daftarBarang')) || [];
+  daftarBarang.splice(index, 1);
+  localStorage.setItem('daftarBarang', JSON.stringify(daftarBarang));
+
+  // Tampilkan kembali daftar barang
+  tampilkanDaftarBarang();
+
+  alert('Data barang berhasil dihapus.');
 }
